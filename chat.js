d36fcdc48c83211cef6ca7c84c45645733f693df
golalaland <golalaland@proton.me>
2026-01-05 09:31:08 +0100

    Update chat.js

--- chat.js
+++ chat.js
@@ -6344,11 +6344,13 @@ function showVotingOptions(poll) {
           stars: increment(poll.reward)
         });
 
-        confetti({
-          particleCount: 150,
-          spread: 70,
-          origin: { y: 0.6 }
-        });
+confetti({
+  particleCount: 150,
+  spread: 70,
+  origin: { y: 0.6 },
+  colors: ['#ff1493', '#ff69b4', '#0f9', '#ffd700'],
+  zIndex: 100000  // This forces it on top
+});
 
         showStarPopup(`Voted for ${option}! +${poll.reward} $STRZ 🎉`);
         showLiveResults(poll, option);
