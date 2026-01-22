e150b0b7332cc724517f8131886c5e366ac40521
golalaland <golalaland@proton.me>
2026-01-22 09:13:34 +0100

    Update chat.js

--- chat.js
+++ chat.js
@@ -5791,7 +5791,7 @@ let allLoadedVideos = [];
 let lastVisibleVideoDoc = null;
 let hasMoreVideos = true;
 let isLoadingVideos = false;
-const VIDEOS_PAGE_SIZE = 3; // your test size — works fine now
+const VIDEOS_PAGE_SIZE = 21; // your test size — works fine now
 const VIDEOS_CACHE_KEY = "highlightsFeedCache_v1";
 
 // Load one page
