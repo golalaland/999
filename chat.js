031a796e2a78f7a122310adf6e94520c23f2cc3c
golalaland <golalaland@proton.me>
2026-05-13 14:29:42 +0100

    Update chat.js

--- chat.js
+++ chat.js
@@ -6850,7 +6850,7 @@ intro.innerHTML = `
         Free Tonight?
       </span>
       
-     <p style="margin:0 0 7px; font-size:15.5px; font-weight:400; color:#b0ffeb; text-shadow: 0 2px 8px rgba(0,0,0,0.6);">
+     <p style="margin:0 0 7px; font-size:13px; font-weight:400; color:#b0ffeb; text-shadow: 0 2px 8px rgba(0,0,0,0.6);">
         Real moments • Real matches • Right now
       </p>
       <p style="margin:0; color:#8899aa; font-size:13.5px;">
